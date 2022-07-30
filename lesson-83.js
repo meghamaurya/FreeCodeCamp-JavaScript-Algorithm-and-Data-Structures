60ea556cc35e72e84ff31feae2cfdc0df75aa638
megha <meghamini40@gmail.com>
2022-07-30 12:05:05 +0530

    Build JavaScript Objects

---
+++ lesson-83.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    name: "kibo",
+    legs: 4,
+    tails: 1,
+    friends: ["megha", 'john', 'tony']
+
+    // Only change code above this line
+};
